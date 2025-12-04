30fd7d204c438771a859488acca79a7b6e25ff8b
Fabio Maulana <fabiomaulana@192.168.1.7>
2025-12-04 14:20:06 +0700

    fix supabase key 2

--- kesimpulan-miniapp/lib/supabaseServer.ts
+++ kesimpulan-miniapp/lib/supabaseServer.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.SUPABASE_URL!; "https://esolvhnpvfoavgycrwgy.supabase.co"
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzb2x2aG5wdmZvYXZneWNyd2d5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQxMzI1MDIsImV4cCI6MjA3OTcwODUwMn0.5Ftxio_WD-2dRhBmMVosu_fYFxjXjhimxLMhZtgHSnY"
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!; "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzb2x2aG5wdmZvYXZneWNyd2d5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDEzMjUwMiwiZXhwIjoyMDc5NzA4NTAyfQ.w_03lST6letbAI-_X266dXd9hH6H0cWLdjCEf-uCoZs"
 
 export const supabaseServer = createClient(supabaseUrl, supabaseServiceKey);
